697d21dd214593f664934612b459c84aaff03672
Jeff-DS <jeff.spingeld+flatironschool@gmail.com>
2016-04-07 17:12:54 -0400

    Allow user to dismiss product list icons with second double-tap

--- FarmersMarketLocator/FarmersMarketLocator/FMLMapViewController.m
+++ FarmersMarketLocator/FarmersMarketLocator/FMLMapViewController.m
@@ -28,6 +28,7 @@
 @property (strong, nonatomic) MKMapView *mapView;
 @property (strong, nonatomic) FMLMapViewDelegate *mapDelegate;
 @property (strong, nonatomic) FMLLocationManagerDelegate *locationDelegate;
+@property (strong, nonatomic) UIView *dimView;
 
 @end
 
@@ -198,6 +199,12 @@
 // When a pin is double-tapped, shoot out an icon for each product available at that Farmer's Market. The icons should radiate out to form a circle around the pin. (If there's not enough room, maybe a spiral instead.)
 -(void)showProductsCircleForMarket:(UITapGestureRecognizer *)gestureRecognizer {
 
+    // To disable and dim the map/background, put a grayish transparent view over it
+    self.dimView = [[UIView alloc] initWithFrame:self.mapView.frame];
+    self.dimView.backgroundColor = [UIColor grayColor];
+    self.dimView.alpha = 0.6; // Could do a blur thing instead of a gray thing, might look nicer
+    [self.view addSubview:self.dimView];
+    
     // Create the view that holds the pin and its icons
     FMLPinAnnotationView *view = (FMLPinAnnotationView *)gestureRecognizer.view;
     // Turn off autoresizing thing (its constraints interfere with ours)
@@ -250,17 +257,20 @@
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     
      To-do:
-     - Disable the map movement and make the background dim.
      - Get actual icons (Noun Project, Flaticon, etc.), put them in Assets, give them the right names, and uncomment the code that adds to the iconsArray using the dictionary. (Could do it without the dictionary and just give them the same names as the strings, BUT there are slashes in some of the strings. Could just change them first though.)
      - Make all the icons circles of uniform size--they don't come as circles, so put them on colored circles. This will be much prettier.
      - Somehow make the center of the circle be the actual location, not just the center of the pin view. More like, the bottom middle?
      - Remove green background from pin view.
+     - Fix the frames thing: should be based on a calculation with screen size, not an absolute number
      - What if there are too many items? Bigger circle? Spiral? A "..." icon that you can tap to expand more?
      - Should it move the double-tapped pin to the center? Otherwise, if it's close to the side some of the icons will be offscreen.
-     - Should there be little text labels next to the icons? Maybe you can touch an icon to see the 
+     - Should there be little text labels next to the icons? Maybe you can touch an icon to see the text
      - On second double-tap (and/or tap elsewhere), make the icons get sucked back into the pin and restore map functionality. (Do this last, because it has to reverse all the previous stuff.)
-     - Decide if we actually need the custom Pin Annotation View subclass for anything.
+     - Instead of making new constraints in the animation, just reassign the values of the existing constraints. Rename appropriately.
     
+     Done:
+     - Disable the map movement and make the background dim.
+     
      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      */
      
@@ -277,7 +287,7 @@
 //    }
     
     // REMOVE THIS AFTER TEST
-    iconsArray = [@[@"Baked goods", @"Baked goods", @"Baked goods", @"Baked goods", @"Baked goods"] mutableCopy];
+    iconsArray = [@[@"Baked goods", @"Baked goods", @"Baked goods", @"Baked goods", @"Baked goods", @"Baked goods", @"Baked goods", @"Baked goods", @"Baked goods", @"Baked goods", @"Baked goods", @"Baked goods", @"Baked goods", @"Baked goods", @"Baked goods", @"Baked goods", @"Baked goods", @"Baked goods"] mutableCopy];
     
     // Place the icons equidistant from each other along a circle around around the pin.
     // Figure out the position of each icon. Basically, divide 360 by the number of icons, and put an icon at a position that many degrees around the circle...?
@@ -289,13 +299,16 @@
     // Make each icon appear at the center of the pin and animate out to its position.
     for (NSString *iconName in iconsArray) {
         
-        // Create the image and a view for it.
+        // ~~~~~ INITIAL STATE OF ICONS ~~~~~
+        
+        // Create the icon and a view for it.
         UIImage *icon = [UIImage imageNamed:iconName];
         UIImageView *iconView = [[UIImageView alloc] initWithImage:icon];
         iconView.contentMode = UIViewContentModeScaleAspectFit;
         iconView.clipsToBounds = YES;
         iconView.translatesAutoresizingMaskIntoConstraints = NO;
-        // Set the image to be sizeless and 100% transparent
+        
+        // Sizeless and fully transparent
         NSLayoutConstraint *zeroHeight = [iconView.heightAnchor constraintEqualToConstant:0];
         zeroHeight.identifier = @"zeroheight";
         NSLayoutConstraint *zeroWidth = [iconView.widthAnchor constraintEqualToConstant:0];
@@ -303,44 +316,68 @@
         zeroHeight.active = YES;
         zeroWidth.active = YES;
         iconView.alpha = 0;
-        // Add the iconView to the mapView as a subview
-        [self.mapView addSubview:iconView];
-        // Set the image view's initial position to be the same as that of the pin, using NSLayoutConstraints.
         
-                NSLayoutConstraint *sameXAsPin = [NSLayoutConstraint constraintWithItem:iconView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:view attribute:NSLayoutAttributeCenterX multiplier:1 constant:0];
+        // Add the iconView to the dimView as a subview
+        [self.dimView addSubview:iconView];
+        
+        // Initial position: centered on the pin, using NSLayoutConstraints.
+        NSLayoutConstraint *sameXAsPin = [NSLayoutConstraint constraintWithItem:iconView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:view attribute:NSLayoutAttributeCenterX multiplier:1 constant:0];
         NSLayoutConstraint *sameYAsPin = [NSLayoutConstraint constraintWithItem:iconView attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:view attribute:NSLayoutAttributeCenterY multiplier:1 constant:0];
         sameXAsPin.active = YES;
         sameYAsPin.active = YES;
 //        iconView.frame = CGRectZero;
         iconView.center = view.center;
+        
+        // ---------------- Duplicate pin view
+        // Add a duplicate pin view in the same place as the old one, so that it can be double-tapped again to get out of this view.
+        FMLPinAnnotationView *newPinView = [[FMLPinAnnotationView alloc] init];
+        newPinView.frame = view.frame;
+        newPinView.pinTintColor = [UIColor yellowColor];
+        [self.dimView addSubview:newPinView];
+        
+        // On second double-tap, get rid of the dim view (this will get rid of the icons and the duplicate pin, which are its subviews)
+        UITapGestureRecognizer *secondDoubleTap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(removeDimViewFromSuperView)];
+        secondDoubleTap.numberOfTapsRequired = 2;
+        secondDoubleTap.delegate = self;
+        [newPinView addGestureRecognizer:secondDoubleTap];
+                                                   
+        
+        // ---------------
+        
         [self.mapView layoutIfNeeded];
+        
+        
+        // ~~~~~ FINAL STATE OF ICONS ~~~~~
         
         // Calculate number of degrees around the circle that the icon should be
         CGFloat degreesForIcon = degreesBetweenIcons * index;
+        
         // Get the coordinates of that position
-        // (RADIUS SUBJECT TO CHANGE, or maybe calculation)
         CGPoint iconDestinationPosition = [self pointAroundCircumferenceFromCenter:view.center withRadius:50 andAngle:degreesForIcon];
+        
         // Animate the icon moving to its position in the circle. Also make it opaque and have size (same size as pin).
         [UIView animateWithDuration:0.25 animations:^{
+            
             // position
-            // TODO: can just re-assign the constraints, rather than making them NO and making new ones.
             sameXAsPin.active = NO;
             sameYAsPin.active = NO;
-            NSLayoutConstraint *xPosition = [iconView.centerXAnchor constraintEqualToAnchor:self.mapView.leftAnchor constant:iconDestinationPosition.x];
-            NSLayoutConstraint *yPosition = [iconView.centerYAnchor constraintEqualToAnchor:self.mapView.topAnchor constant:iconDestinationPosition.y];
+            NSLayoutConstraint *xPosition = [iconView.centerXAnchor constraintEqualToAnchor:self.dimView.leftAnchor constant:iconDestinationPosition.x];
+            NSLayoutConstraint *yPosition = [iconView.centerYAnchor constraintEqualToAnchor:self.dimView.topAnchor constant:iconDestinationPosition.y];
 //            NSLayoutConstraint *xPosition = [NSLayoutConstraint constraintWithItem:iconView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:view attribute:NSLayoutAttributeCenterX multiplier:1 constant:iconDestinationPosition.x];
 //            NSLayoutConstraint *yPosition = [NSLayoutConstraint constraintWithItem:iconView attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:view attribute:NSLayoutAttributeCenterY multiplier:1 constant:iconDestinationPosition.y];
             xPosition.active = YES;
             yPosition.active = YES;
+            
             // alpha (opacity)
             iconView.alpha = 1;
+            
             // size
             zeroHeight.active = NO;
             zeroWidth.active = NO;
             [iconView.heightAnchor constraintEqualToConstant:30].active=YES;
             [iconView.widthAnchor constraintEqualToConstant:30].active=YES;
             // NOTE ON FRAMES: make the height and width a calculation based on the size of the view, so it works correctly regardless of device.
-            iconView.frame = CGRectMake(0, 0, 30, 30);
+//            iconView.frame = CGRectMake(0, 0, 30, 30);
             iconView.center = iconDestinationPosition;
             [self.mapView layoutIfNeeded];
             
@@ -354,6 +391,12 @@
     }
     
 
+}
+
+-(void)removeDimViewFromSuperView{
+    
+    [self.dimView removeFromSuperview];
+    
 }
 
 // Function to get points at a particular number of degrees around a circle
